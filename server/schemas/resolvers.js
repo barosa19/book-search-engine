b3a9178adbcf102642178307e4a3347d59ef9bd0
Brosa <brunoarosa1997@gmail.com>
2023-06-23 12:47:02 -0400

    created addUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,17 @@
+const {User} = require('../models/index')
+const {signToken} = require('../utils/auth')
+
 const resolvers = {
     Query: {
         me: async () => {},
     },
     Mutation: {
         login: async (parent, {email, password}) => {},
-        addUser: async (parent, {username, email, password}) => {},
+        addUser: async (parent, args) => {
+            const user = await User.create({...args})
+            const token = signToken(user)
+            return {token, user}
+        },
         saveBook: async (parent, {bookId, authors, description, title, image, link}) => {},
         removeBook: async (parent, {bookId}) => {},
     }
