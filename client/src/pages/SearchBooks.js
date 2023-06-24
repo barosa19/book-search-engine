78f9712bb4755f5eebfdd9c3741c47c4b893773b
Brosa <brunoarosa1997@gmail.com>
2023-06-23 22:03:32 -0400

    added apollo client and updated signup

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -7,7 +7,8 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
-
+import {useMutation} from '@apollo/client'
+import {SAVE_BOOK} from '../utils/mutation'
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -20,6 +21,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, {error}] = useMutation(SAVE_BOOK)
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
